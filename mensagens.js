0db324b92e63b0022ff6fc749851039045c91c9e
felipemejias <felipe.mejias50@gmail.com>
2022-02-10 16:22:23 -0300

    mensagens carregadas, inicio filtragem

---
+++ mensagens.js
@@ -0,0 +1,8 @@
+dadosCarregamento
+
+function filtrarListaObjetosMensagem(){
+    for(let k=0; k<dadosCarregamento.length;k++){
+        const objetoMensagem=dadosCarregamento[k];
+        
+    }
+}
--- requisicoes.js
+++ requisicoes.js
@@ -3,16 +3,42 @@ postarNomeUsuario();
 
 function postarNomeUsuario(){
     objetoUsuario={name:meuUsuario};
-    const promise=axios.post('https://mock-api.driven.com.br/api/v4/uol/participants',objetoUsuario);
-    promise.then(seDerCerto);
-    promise.catch(seDerErro);
+    const promise= axios.post('https://mock-api.driven.com.br/api/v4/uol/participants',objetoUsuario);
+    promise.then(postagemUsuarioBemSucedida);
+    promise.catch(postagemUsuarioMalSucedida);
 }
 
-function seDerErro(erro){
+function postagemUsuarioMalSucedida(erro){
+    const numeroErro=erro.response.status
+    if(numeroErro==400){
     meuUsuario=prompt('Esse nome já está em uso. Escolha outro:');
     postarNomeUsuario();
 }
+}
+function postagemUsuarioBemSucedida(){
+    console.log('BOOOOOAAA');  // O que fazer quando der certo?
+}
 
-function seDerCerto(){
-    console.log('hjbdehc');
+function manterConexao(){
+    objetoUsuario={name:meuUsuario};
+    const promise= axios.post('https://mock-api.driven.com.br/api/v4/uol/status',objetoUsuario);
+}
+
+const intervaloConexao= setInterval(manterConexao,5000)
+const intervaloMensagens= setInterval(carregarMensagens,3000)
+
+function carregarMensagens(){
+    const promise= axios.get('https://mock-api.driven.com.br/api/v4/uol/messages');
+    promise.then(carregamentoBemSucedido)
+}
+
+let dadosCarregamento=null;
+
+function carregamentoBemSucedido(dados){
+    console.log(dados.data);
+    dadosCarregamento=dados.data
+}
+
+function carregamentoMalSucedido(){
+    console.log('RUUUUUIMMMM')  // O que fazer quando der errado?
 }
